b1389a6ed8753bf4cc5f0d3e231cf304ebf73003
Mantrov Sergei and Didenko Alex <coco6519@mail.ru>
2022-05-16 10:20:43 +0500

    Добавьте файлы проекта.

---
+++ TP2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "1";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text != String.Empty)
+                label1.Text = (Int32.Parse(label1.Text) + 1).ToString();
+
+        }
+    }
+}
